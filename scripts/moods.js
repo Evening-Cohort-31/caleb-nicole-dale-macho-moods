222736bbf36d424e4b610fed5623e47fe6828304
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-08-09 12:58:38 -0500

    added finishing touches to CSS files adding extra commented text about 'tokens' used in main.css

--- scripts/moods.js
+++ scripts/moods.js
@@ -3,11 +3,11 @@ export const moodCard = (moodObject) => {
 
     return `
         <div class="card" id=${id}>
-            <h2>${name}</h2>
+            <h2 class="card__title">${name}</h2>
             <img src=${imageUrl} alt="${name} mood">
             <div class="card__advice">
                 <div class="card__details">
-                    <h3 class="card__advice-label"> Macho Advice:</h3>
+                    <h3 class="card__advice-label"> Macho Advice</h3>
                     <article class="card__advice">${text}</article>
                 </div>
                 <div class="card__quotes">
